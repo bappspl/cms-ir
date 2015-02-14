4f1c6616116fd8295f7d40155a92ebd58b7bf9e0
wtk13 <rybinski05@gmail.com>
2015-02-14 13:37:37 +0100

    404

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -43,6 +43,7 @@ return array(
             'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
+            'layout/error'             => __DIR__ . '/../view/layout/error.phtml',
         ),
         'template_path_stack' => array(
             __DIR__ . '/../view',
